8bbd4f9a6d838d0910ae9c5239e31c303ba333a9
Abdelrahman Ismail <dev@ismail9k.com>
2020-04-25 21:04:05 +0300

    chore: init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Test" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    msg: String,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
