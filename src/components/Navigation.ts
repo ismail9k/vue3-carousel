234709c858626f63700eae06e56762eb0ecc0f41
Abdelrahman Ismail <dev@ismail9k.com>
2020-05-26 17:14:58 +0300

    refactor: convert Navigation and Pagination into functional components

--- src/components/Navigation.ts
+++ src/components/Navigation.ts
@@ -1,33 +1,24 @@
-import { defineComponent, inject, h } from 'vue';
+import { inject, h } from 'vue';
 
 import Icon from './Icon';
 
-import { SetupContext, CarouselNav } from '../types';
+import { CarouselNav } from '../types';
 
-export default defineComponent({
-  name: 'Navigation',
-  setup(_, { emit }: SetupContext) {
-    const nav: CarouselNav = inject('nav', {});
+const Navigation = () => {
+  const nav: CarouselNav = inject('nav', {});
 
-    function handleNextClick(): void {
-      nav.next();
-      emit('next');
-    }
-    function handlePrevClick(): void {
-      nav.prev();
-      emit('prev');
-    }
+  const prevButton = h(
+    'button',
+    { class: 'carousel__prev', onClick: nav.prev },
+    h(Icon, { name: 'arrowLeft' })
+  );
+  const nextButton = h(
+    'button',
+    { class: 'carousel__next', onClick: nav.next },
+    h(Icon, { name: 'arrowRight' })
+  );
 
-    const prevButton = h(
-      'button',
-      { class: 'carousel__prev', onClick: handlePrevClick },
-      h(Icon, { name: 'arrowLeft' })
-    );
-    const nextButton = h(
-      'button',
-      { class: 'carousel__next', onClick: handleNextClick },
-      h(Icon, { name: 'arrowRight' })
-    );
-    return () => h('div', [prevButton, nextButton]);
-  },
-});
+  return h('div', [prevButton, nextButton]);
+};
+
+export default Navigation;
--- src/components/Pagination.ts
+++ src/components/Pagination.ts
@@ -1,35 +1,30 @@
-import { defineComponent, inject, ref, h } from 'vue';
+import { inject, ref, h } from 'vue';
 
-import { SetupContext, CarouselNav, VNode } from '../types';
+import { CarouselNav, VNode } from '../types';
 
-export default defineComponent({
-  name: 'Pagination',
-  setup(_, { emit }: SetupContext) {
-    const slidesCount = inject('slidesCount', ref(1));
-    const currentSlide = inject('currentSlide', ref(1));
-    const nav: CarouselNav = inject('nav', {});
+const Pagination = () => {
+  const slidesCount = inject('slidesCount', ref(1));
+  const currentSlide = inject('currentSlide', ref(1));
+  const nav: CarouselNav = inject('nav', {});
 
-    function handleButtonClick(slideNumber: number): void {
-      nav.slideTo(slideNumber);
-      emit('slide', slideNumber);
-    }
+  function handleButtonClick(slideNumber: number): void {
+    nav.slideTo(slideNumber);
+  }
 
-    function getChildren() {
-      const children: Array<VNode> = [];
-      for (let slide = 0; slide < slidesCount.value; slide++) {
-        const button = h('button', {
-          class: {
-            'carousel__pagination-button': true,
-            'carousel__pagination-button--active': currentSlide.value === slide,
-          },
-          onClick: () => handleButtonClick(slide),
-        });
-        const item = h('li', { class: 'carousel__pagination-item', key: slide }, button);
-        children.push(item);
-      }
-      return children;
-    }
+  const children: Array<VNode> = [];
+  for (let slide = 0; slide < slidesCount.value; slide++) {
+    const button = h('button', {
+      class: {
+        'carousel__pagination-button': true,
+        'carousel__pagination-button--active': currentSlide.value === slide,
+      },
+      onClick: () => handleButtonClick(slide),
+    });
+    const item = h('li', { class: 'carousel__pagination-item', key: slide }, button);
+    children.push(item);
+  }
 
-    return () => h('ol', { class: 'carousel__pagination' }, getChildren());
-  },
-});
+  return h('ol', { class: 'carousel__pagination' }, children);
+};
+
+export default Pagination;
