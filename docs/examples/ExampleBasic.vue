2d623cde6ee5467903b2b69cd7ea5b311155d515
ismail9k <dev@ismail9k.com>
2021-02-21 13:47:43 +0300

    docs: update example docs

---
+++ docs/examples/ExampleBasic.vue
@@ -0,0 +1,29 @@
+<template>
+  <Carousel>
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">{{ slide }}</div>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Pagination, Navigation, Slide } from '../../dist/carousel.es';
+
+import '../../dist/carousel.css';
+
+export default defineComponent({
+  name: 'Basic',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+});
+</script>
---
+++ docs/examples/ExampleBreakpoints.vue
@@ -0,0 +1,48 @@
+<template>
+  <Carousel :settings="settings" :breakpoints="breakpoints">
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">{{ slide }}</div>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+    </template>
+  </Carousel>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { Carousel, Navigation, Slide } from '../../dist/carousel.es';
+
+import '../../dist/carousel.css';
+
+export default defineComponent({
+  name: 'Breakpoints',
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  },
+  data: () => ({
+    // carousel settings
+    settings: {
+      itemsToShow: 1,
+      snapAlign: 'center',
+    },
+    // breakpoints are mobile first
+    // any settings not specified will fallback to the carousel settings
+    breakpoints: {
+      // 700px and up
+      700: {
+        itemsToShow: 3.5,
+        snapAlign: 'center',
+      },
+      // 1024 and up
+      1024: {
+        itemsToShow: 5,
+        snapAlign: 'start',
+      },
+    },
+  }),
+});
+</script>
