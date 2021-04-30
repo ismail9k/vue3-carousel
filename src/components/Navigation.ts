8c0c1ae2547f7fd58893e944c22b99f3436bbf97
Michal Paukert <paukert.michal@gmail.com>
2021-04-30 07:30:25 +0200

    Update Navigation.ts

--- src/components/Navigation.ts
+++ src/components/Navigation.ts
@@ -4,13 +4,13 @@ import Icon from './Icon';
 
 import { CarouselNav } from '../types';
 
-const Navigation = (props: any, { slots }: any) => {
+const Navigation = (props: any, { slots, attrs }: any) => {
   const { next: slotNext, prev: slotPrev } = slots;
   const nav: CarouselNav = inject('nav', {});
 
   const prevButton = h(
     'button',
-    { class: 'carousel__prev', onClick: nav.prev },
+    { class: ['carousel__prev', attrs?.class] , onClick: nav.prev },
     slotPrev?.() || h(Icon, { name: 'arrowLeft' })
   );
   const nextButton = h(
