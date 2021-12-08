636cf7141b434daab3797155817180541ae677e2
Piotr Radzikowski <ptr.radzikowski@gmail.com>
2021-12-08 15:23:29 +0100

    Add optionaly css class to hide in active navigation arrows

--- src/components/Navigation.ts
+++ src/components/Navigation.ts
@@ -1,18 +1,27 @@
-import { inject, h } from 'vue';
+import { inject, ref, h, reactive } from 'vue';
 
 import Icon from './Icon';
+import { defaultConfigs } from '@/partials/defaults';
 
-import { CarouselNav } from '../types';
+import { CarouselNav, CarouselConfig } from '../types';
 
 const Navigation = (props: any, { slots, attrs }: any) => {
   const { next: slotNext, prev: slotPrev } = slots;
+  const config: CarouselConfig = inject('config', reactive({ ...defaultConfigs }));
+  const maxSlide = inject('maxSlide', ref(1));
+  const minSlide = inject('minSlide', ref(1));
+  const currentSlide = inject('currentSlide', ref(1));
   const nav: CarouselNav = inject('nav', {});
 
   const prevButton = h(
     'button',
     {
       type: 'button',
-      class: ['carousel__prev', attrs?.class],
+      class: [
+        'carousel__prev',
+        (!config.wrapAround && currentSlide.value <= minSlide.value) && 'carousel__prev--in-active',
+        attrs?.class
+      ],
       'aria-label': `Navigate to previous slide`,
       onClick: nav.prev,
     },
@@ -22,7 +31,11 @@ const Navigation = (props: any, { slots, attrs }: any) => {
     'button',
     {
       type: 'button',
-      class: ['carousel__next', attrs?.class],
+      class: [
+        'carousel__next',
+        (!config.wrapAround && currentSlide.value >= maxSlide.value) && 'carousel__next--in-active',
+        attrs?.class
+      ],
       'aria-label': `Navigate to next slide`,
       onClick: nav.next,
     },
