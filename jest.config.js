2fd1c986cf2184402073faf05efa29610a456a1a
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-07-11 16:26:59 +0300

    chore: fix types generation
    Now index.d.ts should be directly in dist folder, and it wont be ignored

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,9 @@ module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>',
   }),
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 };
