f1b2088ac6b10b09560679007f1aa0ef28bc49ba
ismail9k <dev@ismail9k.com>
2022-08-09 19:35:51 +0300

    chore: update ts types output #184

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,48 @@
-import css from 'rollup-plugin-css-only';
-import typescript from 'rollup-plugin-typescript2';
+import css from 'rollup-plugin-css-only'
+import typescript from 'rollup-plugin-typescript2'
+import del from 'rollup-plugin-delete'
+import dts from 'rollup-plugin-dts'
 
-import pkg from './package.json';
+import pkg from './package.json'
 
 const banner = `/**
  * Vue 3 Carousel ${pkg.version}
  * (c) ${new Date().getFullYear()}
  * @license MIT
- */`;
+ */`
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'umd',
-      name: 'VueCarousel',
-      banner,
-      globals: {
-        vue: 'Vue',
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'umd',
+        name: 'VueCarousel',
+        banner,
+        globals: {
+          vue: 'Vue',
+        },
       },
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      banner,
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    css({ output: 'carousel.css' }),
-    typescript({
-      typescript: require('typescript'),
-    }),
-  ],
-};
+      {
+        file: pkg.module,
+        format: 'es',
+        banner,
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      css({ output: 'carousel.css' }),
+      typescript({ useTsconfigDeclarationDir: true }),
+    ],
+  },
+  {
+    input: 'dist/dts/index.d.ts',
+    output: [{ file: 'dist/carousel.d.ts', format: 'es' }],C
+    external: [/\.css$/],
+    plugins: [dts(), del({ hook: 'buildEnd', targets: 'dist/dts', runOnce: true })],
+  },
+]
