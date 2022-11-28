9fa7a9ae717b7dbfb1cfc8116355b0f426e21319
babu-ch <mashirohira@gmail.com>
2022-11-28 17:16:32 +0900

    bugfix #248

--- src/utils/getSlidesVNodes.ts
+++ src/utils/getSlidesVNodes.ts
@@ -3,7 +3,8 @@ export function getSlidesVNodes(vNode: any[] | undefined) {
   if (!vNode) return []
 
   // Check if the Slides components are added directly without v-for (#72)
-  if (vNode[0]?.type?.name === 'CarouselSlide') return vNode
+  if (vNode[0]?.children === "v-if" || vNode[0]?.type?.name === 'CarouselSlide') return vNode.filter((node: any) => node.type?.name === "CarouselSlide")
+
 
   return vNode[0]?.children || []
 }
