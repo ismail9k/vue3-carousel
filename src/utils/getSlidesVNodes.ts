639e071048cd55a0d50229d2fd6c1cd63391f9ac
ismail9k <dev@ismail9k.com>
2023-04-02 11:45:32 +0300

    enhance getSlidesVNodes logic

--- src/utils/getSlidesVNodes.ts
+++ src/utils/getSlidesVNodes.ts
@@ -1,10 +1,17 @@
-export function getSlidesVNodes(vNode: any[] | undefined) {
-  // Return empty array if there's any node
+import { Fragment } from 'vue'
+
+export function getSlidesVNodes(vNode?: any[]): any {
   if (!vNode) return []
 
-  // Check if the Slides components are added directly without v-for (#72)
-  if (vNode[0]?.children === "v-if" || vNode[0]?.type?.name === 'CarouselSlide') return vNode.filter((node: any) => node.type?.name === "CarouselSlide")
+  return vNode.reduce((acc, node) => {
+    if (node.type === Fragment) {
+      return [...acc, ...getSlidesVNodes(node.children)]
+    }
 
+    if (node.type?.name === 'CarouselSlide') {
+      return [...acc, node]
+    }
 
-  return vNode[0]?.children || []
+    return acc
+  }, [])
 }
