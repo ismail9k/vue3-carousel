9a889f868d9f98218830adb145204c2e2c4068f7
ismail9k <dev@ismail9k.com>
2024-10-29 11:40:30 +0300

    chore(docs): config google analytics

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -30,4 +30,19 @@ module.exports = {
       },
     ],
   },
+
+  head: [
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-37DX3WR4T0',
+      },
+    ],
+    [
+      'script',
+      {},
+      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-37DX3WR4T0');",
+    ],
+  ],
 }
