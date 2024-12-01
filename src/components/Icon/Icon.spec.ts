88e3db8ebb9d20b63bd0b162c26138e0443e9b76
Ismail9k <dev@ismail9k.com>
2024-12-01 14:05:22 +0300

    refactor: rename icon.spec.ts to Icon.spec.ts and improve test structure

--- src/components/Icon/Icon.spec.ts
+++ src/components/Icon/Icon.spec.ts
@@ -1,9 +1,8 @@
-import { expect, it, describe, beforeAll, afterEach, vi } from 'vitest'
-
 import { mount } from '@vue/test-utils'
+import { expect, it, describe, afterEach, vi } from 'vitest'
 
-import { Icon } from '@/index'
-import { IconProps } from '@/components/Icon'
+import { Icon } from './Icon'
+import { IconName, IconProps } from './Icon.types'
 
 describe('Icon.ts', () => {
   const consoleMock = vi.spyOn(console, 'warn').mockImplementation(() => undefined)
@@ -20,11 +19,19 @@ describe('Icon.ts', () => {
   })
 
   it('It should error if iconName is invalid', () => {
-    const wrapper = mount(Icon, { props: { name: 'foo' } })
+    const wrapper = mount(Icon, { props: { name: 'foo' as any } })
     expect(wrapper.html()).toBe('')
     expect(consoleMock).toHaveBeenCalledOnce()
     expect(consoleMock.mock.calls[0][0]).toBe(
       '[Vue warn]: Invalid prop: custom validator check failed for prop "name".'
     )
+  })
+
+  it('It should render standalone', () => {
+    Object.values(IconName).forEach((name) => {
+      const wrapper = mount(Icon, { props: { name: name } as IconProps })
+      expect(consoleMock).not.toHaveBeenCalled()
+      expect(wrapper.html()).toMatchSnapshot()
+    })
   })
 })
