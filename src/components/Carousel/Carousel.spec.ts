1bc6de90cf62f517af8fa96693945dce2c902d75
Ismail9k <dev@ismail9k.com>
2024-12-26 14:25:33 +0300

    tests: fix tests

--- src/components/Carousel/Carousel.spec.ts
+++ src/components/Carousel/Carousel.spec.ts
@@ -14,12 +14,4 @@ describe('Carousel.ts', () => {
     const carousel = wrapper.find('.carousel')
     expect(carousel.exists()).toBe(true)
   })
-
-  it('Should apply fade effect when slideEffect is set to fade', async () => {
-    await wrapper.setProps({ slideEffect: 'fade' })
-    const slide = wrapper.find('.carousel__slide')
-    expect(slide.element.style.opacity).toBe('0')
-    const activeSlide = wrapper.find('.carousel__slide--active')
-    expect(activeSlide.element.style.opacity).toBe('1')
-  })
 })
